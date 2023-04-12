e1efe73d9f447c59cadfae2869cb3c7d91f2291b
Giau Le <ltgiau91@gmail.com>
2023-04-12 23:42:41 +0700

    docker demo

---
+++ simple-demo-withdocker/src/test/java/com/helen/demo/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
