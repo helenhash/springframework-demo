ed5b72d9f6b5412295ea94a3f9c6a397c156923b
helenhash <ltgiau91@gmail.com>
2024-08-01 22:47:10 +0700

    Feat: Unit test

--- data-jpa-demo/src/test/java/com/helen/demo/service/ProductServiceTestV1.java
+++ data-jpa-demo/src/test/java/com/helen/demo/service/ProductServiceTestV1.java
@@ -1,24 +1,18 @@
 package com.helen.demo.service;
 
-import com.helen.demo.DataJpaDemoApplication;
+import com.helen.demo.DataJPADemoApplication;
 import com.helen.demo.entity.Product;
 import com.helen.demo.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest(classes = DataJpaDemoApplication.class)
+@SpringBootTest(classes = DataJPADemoApplication.class)
 class ProductServiceTestV1 {
 
 	@Autowired
